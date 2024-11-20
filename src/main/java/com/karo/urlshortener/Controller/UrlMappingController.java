7a4ab7a9da182adc532fd543119682072db54266
Karok365 <anshsinghh365@gmail.com>
2024-11-21 00:55:31 +0530

    Controller Created

---
+++ src/main/java/com/karo/urlshortener/Controller/UrlMappingController.java
@@ -0,0 +1,26 @@
+package com.karo.urlshortener.Controller;
+
+import com.karo.urlshortener.Service.UrlMappingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/gg")
+public class UrlMappingController {
+    @Autowired
+    private UrlMappingService urlMappingService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateShortUrl(@RequestBody String originalUrl) {
+        String shortUrl = urlMappingService.generateShortUrl(originalUrl);
+        urlMappingService.saveUrlMapping(originalUrl, shortUrl);
+        return ResponseEntity.ok(shortUrl);
+    }
+
+    @GetMapping("/{shortUrl}")
+    public ResponseEntity<String> redirectToOriginalUrl(@PathVariable String shortUrl) {
+        String originalUrl = urlMappingService.getOriginalUrl(shortUrl);
+        return ResponseEntity.status(302).header("Location", originalUrl).build();
+    }
+}
