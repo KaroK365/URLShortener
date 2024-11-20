412a257c8f4aefe4a26dff4fa0273d32b2e43beb
Karok365 <anshsinghh365@gmail.com>
2024-11-20 23:42:04 +0530

    Url Mapping Repository Test

--- src/main/java/com/karo/urlshortener/Repository/UrlMappingRepository.java
+++ src/main/java/com/karo/urlshortener/Repository/UrlMappingRepository.java
@@ -4,4 +4,6 @@ import com.karo.urlshortener.Entity.UrlMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UrlMappingRepository extends JpaRepository<UrlMapping, Integer> {
+    UrlMapping findByoriginalUrl(String originalUrl);
+    UrlMapping findByshortUrl(String shortUrl);
 }
---
+++ src/test/java/com/karo/urlshortener/UrlMappingRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.karo.urlshortener;
+
+
+import com.karo.urlshortener.Entity.UrlMapping;
+import com.karo.urlshortener.Repository.UrlMappingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class UrlMappingRepositoryTest {
+    @Autowired
+    private UrlMappingRepository urlMappingRepository;
+
+    public void testSaveandFindByShortUrl() {
+        UrlMapping urlMapping = new UrlMapping();
+        urlMapping.setShortUrl("short");
+        urlMapping.setOriginalUrl("original");
+        urlMapping.setCreatedAt(LocalDateTime.now());
+
+        UrlMapping saved = urlMappingRepository.save(urlMapping);
+        UrlMapping found = urlMappingRepository.findByshortUrl("short");
+
+        assertThat(found).isNotNull();
+        assertThat(found.getOriginalUrl()).isEqualTo("original");
+        assertThat(found.getShortUrl()).isEqualTo("short");
+    }
+}
