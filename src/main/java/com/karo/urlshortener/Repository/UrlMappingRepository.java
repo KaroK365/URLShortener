c88244ba6da8f0e5aa79617be368c02a6d9eeca8
Karok365 <anshsinghh365@gmail.com>
2024-11-20 23:09:11 +0530

    Url Mapping Repository

--- src/main/java/com/karo/urlshortener/Repository/UrlMappingRepository.java
+++ src/main/java/com/karo/urlshortener/Repository/UrlMappingRepository.java
@@ -1,4 +1,7 @@
 package com.karo.urlshortener.Repository;
 
-public class UrlMappingRepository {
+import com.karo.urlshortener.Entity.UrlMapping;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UrlMappingRepository extends JpaRepository<UrlMapping, Integer> {
 }
