4227f0601774957ddb110aaddba764772d729304
Karok365 <anshsinghh365@gmail.com>
2024-11-21 00:06:17 +0530

    Service complete

---
+++ src/main/java/com/karo/urlshortener/Service/UrlMappingService.java
@@ -0,0 +1,32 @@
+package com.karo.urlshortener.Service;
+
+
+import com.karo.urlshortener.Entity.UrlMapping;
+import com.karo.urlshortener.Repository.UrlMappingRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class UrlMappingService {
+    private UrlMappingRepository urlMappingRepository;
+
+    public String generateShortUrl(String originalUrl) {
+        return UUID.randomUUID().toString().substring(0,6);
+    }
+
+    public UrlMapping saveUrlMapping(String originalUrl, String shortUrl) {
+        UrlMapping urlMapping = new UrlMapping();
+        urlMapping.setOriginalUrl(originalUrl);
+        urlMapping.setShortUrl(shortUrl);
+        return urlMappingRepository.save(urlMapping);
+    }
+
+    public String getOriginalUrl(String shortUrl) {
+        UrlMapping urlMapping = urlMappingRepository.findByshortUrl(shortUrl);
+        if(urlMapping != null) {
+            return urlMapping.getOriginalUrl();
+        }
+        throw new IllegalArgumentException("Short Url Not Found");
+    }
+}
